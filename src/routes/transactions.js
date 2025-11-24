9d8fdc12e6bf8db14f9bb37771dfa3244c015a3e
abcryk12-afk <abcryk12@gmail.com>
2025-11-24 13:55:36 +0500

    Add files via upload

---
+++ src/routes/transactions.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { authRequired } = require('../middleware/auth');
+const { transactions } = require('../data/store');
+
+const router = express.Router();
+
+router.get('/', authRequired, (req, res) => {
+  const userTransactions = transactions.filter((t) => t.userId === req.user.id);
+  
+  res.json({
+    transactions: userTransactions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+  });
+});
+
+module.exports = router;
